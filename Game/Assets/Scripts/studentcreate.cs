627fd6fd68230d197e50b3e8d92cbe2cb4fb8fc6
Ali Eren Kayhan <ali_erenbatman@hotmail.com>
2023-04-08 18:10:47 +0300

    postprocessing-studentsmove-capacityupdate
    postprocessing-studentsmove-capacityupdate

--- Game/Assets/Scripts/studentcreate.cs
+++ Game/Assets/Scripts/studentcreate.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +6,34 @@ public class studentcreate : MonoBehaviour
 {
     [SerializeField] int stundentsnumber = 0;
     [SerializeField] int maxstundentsnumber = 0;
+    [SerializeField] float durationbetweenstudentscreation;
 
     [SerializeField] GameObject student;
 
     [SerializeField] float studentprefabx;
     [SerializeField] float studentprefaby;
-    private void Start()
+
+
+    IEnumerator Start()
     {
         for (; stundentsnumber < maxstundentsnumber; stundentsnumber++)
         {
             if (stundentsnumber != maxstundentsnumber)
             {
-                Instantiate(student, new Vector2(studentprefabx, studentprefaby), Quaternion.identity);
+                yield return StartCoroutine(Createstudent(durationbetweenstudentscreation));
             }
         }
+        //print("Done " + Time.time);
+
+    }
+
+    IEnumerator Createstudent(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        Instantiate(student, new Vector2(studentprefabx, studentprefaby), Quaternion.identity);
+
+        //print("Createstudent " + Time.time);
+
     }
 }
--- Game/Assets/Scripts/studentrandomwalk.cs
+++ Game/Assets/Scripts/studentrandomwalk.cs
@@ -1,10 +1,37 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class studentrandomwalk : MonoBehaviour
 {
-    //There will be scripts about random walking for stundents
+
+    public waypoints waypointscript;
+    public int waypointindex;
+    public float distance;
+    public float speed;
+
+    private void Awake()
+    {
+        waypointscript = GameObject.Find("GameManager").GetComponent<waypoints>();
+    }
+
+    private void Start()
+    {
+        waypointindex = Random.Range(0, 4);
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypointscript.waypoint[waypointindex].transform.position,speed*Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, waypointscript.waypoint[waypointindex].transform.position) < distance)
+        {
+            waypointindex = Random.Range(0, 4);
+        }
+    }
+
+
 
 }
+ 
